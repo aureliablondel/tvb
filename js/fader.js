8793220fb2a59428921d5f396aaddf54b3572324
Aurelia BLONDEL <aureliablondel56@gmail.com>
2021-06-22 17:31:17 +0200

    last change

--- js/fader.js
+++ js/fader.js
@@ -1,6 +1,7 @@
-//  Slideshow avec fondu enchaîné
+//  Slideshow avec fondu enchaîné pour écran > 1024px
 
 $('.slides img').slice(0).hide();
-    setInterval(function(){
+if($(window).width()>1024){
+    setInterval(function(){      
       $('.slides > :first-child').fadeOut(1500).next('img').fadeIn(1500).end().appendTo('.slides');
-      },4000);
+      },4000);}
